08378d2602b3069e440fdef3a3a3c9eba74a8e40
afeigin <afeigin@nvidia.com>
2025-02-06 12:14:22 +0200

    Enhance show platform summary test

--- tests/show_platform_test.py
+++ tests/show_platform_test.py
@@ -28,7 +28,8 @@ class TestShowPlatform(object):
     TEST_ASIC_TYPE = "mellanox"
     TEST_ASIC_COUNT = 1
     TEST_SERIAL = "MT1822K07815"
-    TEST_MODEL = "MSN2700-CS2FO"
+    TEST_MODEL = "N/A"
+    TEST_PART = "MSN2700-CS2FO"
     TEST_REV = "A1"
 
     # Test 'show platform summary'
@@ -40,13 +41,14 @@ class TestShowPlatform(object):
             ASIC Count: {}
             Serial Number: {}
             Model Number: {}
+            Part Number: {}
             Hardware Revision: {}
-            """.format(self.TEST_PLATFORM, self.TEST_HWSKU, self.TEST_ASIC_TYPE, self.TEST_ASIC_COUNT, self.TEST_SERIAL, self.TEST_MODEL, self.TEST_REV)
+            """.format(self.TEST_PLATFORM, self.TEST_HWSKU, self.TEST_ASIC_TYPE, self.TEST_ASIC_COUNT, self.TEST_SERIAL, self.TEST_MODEL, self.TEST_PART, self.TEST_REV)
 
         with mock.patch("sonic_py_common.device_info.get_platform_info",
                 return_value={"platform": self.TEST_PLATFORM, "hwsku": self.TEST_HWSKU, "asic_type": self.TEST_ASIC_TYPE, "asic_count": self.TEST_ASIC_COUNT}):
             with mock.patch("show.platform.get_chassis_info",
-                            return_value={"serial": self.TEST_SERIAL, "model": self.TEST_MODEL, "revision": self.TEST_REV}):
+                            return_value={"serial": self.TEST_SERIAL, "sys_display": self.TEST_MODEL, "model": self.TEST_PART, "revision": self.TEST_REV}):
                 result = CliRunner().invoke(show.cli.commands["platform"].commands["summary"], [])
                 assert result.output == textwrap.dedent(expected_output)
 
